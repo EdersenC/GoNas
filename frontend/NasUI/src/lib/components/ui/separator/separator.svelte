588542f27fca79ab4324a324b3ca88c8f0a9bd3c
Edersen Cadeau <Edersenc@gmail.com>
2026-01-16 17:34:16 -0500

    quick push

---
+++ frontend/NasUI/src/lib/components/ui/separator/separator.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Separator as SeparatorPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils.js";
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		"data-slot": dataSlot = "separator",
+		...restProps
+	}: SeparatorPrimitive.RootProps = $props();
+</script>
+
+<SeparatorPrimitive.Root
+	bind:ref
+	data-slot={dataSlot}
+	class={cn(
+		"bg-border/100 shrink-0 data-[orientation=horizontal]:h-[2px] data-[orientation=vertical]:min-h-full data-[orientation=vertical]:w-[0.04px]",
+		className
+	)}
+	{...restProps}
+/>
